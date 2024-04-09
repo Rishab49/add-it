97c6b67dbfdcb6109e0308f8b16d9332f46808c4
Rishab49 <rishab.rishu49@gmail.com>
2024-04-09 09:14:11 +0530

    UI update - 2

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 import { useEffect, useRef, useState } from "react";
 import { Master, Matrix, SelectedElementStack, } from "../app";
-
+import StarIcon from "@/assets/star.png";
+import ClockIcon from "@/assets/clock.png";
+import Image from "next/image";
 export default function Home() {
 
 
@@ -29,7 +31,7 @@ export default function Home() {
                     selectedElements.stack.forEach(e => {
                         e.element.style.background = "";
                         e.element.style.backgroundColor = `hsla(${360 * (Number(e.element.innerText) / 100)},100%,50%)`;
-                        e.element.style.color="black";
+                        e.element.style.color = "black";
                     });
                     console.log("else executed", master.currentAddition == master.getCurrentTarget)
                     setSelectedElements(new SelectedElementStack([]));
@@ -89,21 +91,37 @@ export default function Home() {
 
     return (
         <main className="flex flex-col items-stretch justify-stretch h-full w-full relative">
-            <div className="absolute top-0 left-0">
-                <div 
-                className={`h-10 w-10 bg-red-600 `}
+            <div className="absolute top-0 left-0 h-screen w-screen z-[-10]"
                 style={{
-                    transform:`translateX(${Math.round(Math.random() * 100)}vw) translateY(${Math.round(Math.random() * 100)}vh)`
+                    background: "repeating-linear-gradient(0deg,rgba(255,255,255,0.25) 0px 1px,transparent 1px 20px),repeating-linear-gradient(90deg,rgba(255,255,255,0.25) 0px 1px,transparent 1px 20px)"
                 }}
-                ></div>
+            >
             </div>
-            <div className="flex items-center justify-between h-[60px] w-full p-4">
-                <span>Time : 33</span>
-                <span>Score : {master instanceof Master && master.score}</span>
+            <div className="absolute top-0 right-0 flex flex-col items-center justify-end gap-4 h-fit w-fit p-4">
+
+                <span className="flex h-[50px] w-fit">
+                    <Image alt="star" src={StarIcon} height="45" width="45" className="p-2 z-10 bg-[#272a57]  bg-yellow rounded-md border-orange-400 border-2" />
+                    <p className="flex items-center justify-center p-2 rounded-md bg-orange-400 w-[50px] -translate-x-3 text-2xl relative before:content-[''] before:absolute before:top-[5px] before:right-[5px] before:bg-[#ffffff5c] before:h-[10px] before:w-[15px]  before:rounded-full before:rotate-45" style={{
+                        boxShadow: `rgba(0, 0, 0, 0.25) 0px -5px inset`
+                    }}>{master instanceof Master && master.score}</p>
+                </span>
+                <span className="flex h-[50px] w-fit">
+                    <Image alt="star" src={ClockIcon} height="45" width="45" className="aspect-square p-2 z-10 bg-[#272a57]  bg-yellow rounded-md border-orange-400 border-2" />
+                    <p className="flex items-center justify-center p-2 rounded-md bg-orange-400 w-[50px] -translate-x-3 text-2xl relative before:content-[''] before:absolute before:top-[5px] before:right-[5px] before:bg-[#ffffff5c] before:h-[10px] before:w-[15px]  before:rounded-full before:rotate-45" style={{
+                        boxShadow: `rgba(0, 0, 0, 0.25) 0px -5px inset`
+                    }}>{master instanceof Master && master.score}</p>
+                </span>
             </div>
             <div className="flex items-center justify-center flex-col flex-1 gap-20">
-                <div className="text-xl">
-                    {master instanceof Master && master.currentAddition} = {master instanceof Master && master.getCurrentTarget}
+                <div className="flex">
+                    <p className="flex items-center justify-center p-2 rounded-md bg-orange-400 w-[50px] translate-x-3 text-2xl relative before:content-[''] before:absolute before:top-[5px] before:right-[5px] before:bg-[#ffffff5c] before:h-[10px] before:w-[15px]  before:rounded-full before:rotate-45" style={{
+                        boxShadow: `rgba(0, 0, 0, 0.25) 0px -5px inset`
+                    }}>{master instanceof Master && master.score}</p>
+                    <div className="p-2 z-10 bg-[#272a57]  bg-yellow rounded-md border-orange-400 border-2 text-white" > = </div>
+                    <p className="flex items-center justify-center p-2 rounded-md bg-orange-400 w-[50px] -translate-x-3 text-2xl relative before:content-[''] before:absolute before:top-[5px] before:right-[5px] before:bg-[#ffffff5c] before:h-[10px] before:w-[15px]  before:rounded-full before:rotate-45" style={{
+                        boxShadow: `rgba(0, 0, 0, 0.25) 0px -5px inset`
+                    }}>{master instanceof Master && master.score}</p>
+
                 </div>
                 <div className="gap-[0.15rem] flex border-[4px] border-black rounded-lg flex-row h-[315.2px] w-[315.2px] items-end justify-end overflow-hidden" ref={gameContainer}>
 
@@ -117,7 +135,7 @@ export default function Home() {
                                     return <div
                                         data-id={`${index}-${ind}-${row.id}`}
                                         key={`${index}-${row.id}=${row.value}`}
-                                        className="flex items-center justify-center text-3xl h-[75px] w-[75px] hover:bg-orange-400 select-none duration-[500ms] overflow-hidden rounded-md"
+                                        className="relative flex items-center justify-center text-3xl h-[75px] w-[75px] hover:bg-orange-400 select-none duration-[500ms] overflow-hidden rounded-md before:content-[''] before:absolute before:top-[5px] before:left-[5px] before:bg-[#ffffff5c] before:h-[10px] before:w-[15px]  before:rounded-full before:-rotate-45"
                                         onMouseDown={(e) => {
                                             master instanceof Master && setMaster((master: Master) => master.resetCurrentAddition());
                                             // setCurrentAddition(0);
@@ -138,7 +156,7 @@ export default function Home() {
                                         onMouseUp={onMouseUp}
                                         style={{
                                             backgroundColor: `hsla(${360 * (matrix.elements[index][ind].value / 100)},100%,50%)`,
-                                            boxShadow:`0px -5px rgba(0,0,0,0.25) inset`
+                                            boxShadow: `0px -5px rgba(0,0,0,0.25) inset`
                                         }}
                                     >{matrix.elements[index][ind].value}</div>
                                 })}
